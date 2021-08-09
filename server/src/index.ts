bc1350f73583002a0aaca588bda1e445ad9004a5
Brian Hang <hangwithbrian@gmail.com>
2021-08-08 18:27:52 -0700

    Add Koa router

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,15 @@
-console.log('TODO');
+import Koa from 'koa'
+import Router from 'koa-router';
+
+const app = new Koa();
+const router = new Router();
+
+router.get('/greeting', async ctx => {
+  ctx.body = 'Hello, World!';
+});
+
+app.use(router.routes());
+
+const port = app.env === 'development' ? 3001 : 3000;
+app.listen(port);
+console.info(`Started web server on port ${port}`);
