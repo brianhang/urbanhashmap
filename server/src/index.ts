a578b57f3202fd39d976a267c10c791fbc6a7624
Brian Hang <hangwithbrian@gmail.com>
2021-09-25 14:03:02 -0700

    Move https to front end server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,3 @@
-import * as fs from 'fs/promises';
-import * as https from 'https';
-
 import Koa from 'koa';
 import Router from 'koa-router';
 import type User from './models/User';
@@ -26,29 +23,6 @@ app.use(router.routes());
 
 setupDatabase();
 
-async function startServer() {
-  let key = null;
-  let cert = null;
-  try {
-    [cert, key] = await Promise.all([
-      fs.readFile(process.env.TLS_CERT_PATH!),
-      fs.readFile(process.env.TLS_KEY_PATH!),
-    ]);
-  } catch (error) {
-    console.error('Failed to load key or cert file:', error);
-    return;
-  }
-  const port = Number.parseInt(process.env.APP_PORT ?? '') ?? 3000;
-  const serverOptions = {
-    cert,
-    key,
-  };
-  try {
-    https.createServer(serverOptions, app.callback()).listen(port);
-    console.info(`Started web server on port ${port}`);
-  } catch (error) {
-    console.error('Failed to start web server:', error);
-  }
-}
-
-startServer();
+const port = Number.parseInt(process.env.APP_PORT ?? '') ?? 3000;
+app.listen(port);
+console.info(`Started web server on port ${port}`);
